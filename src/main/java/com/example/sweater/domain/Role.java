5d34f6336303baaa16b6f3a1651bd2711a4d0607
Niporka <pavelniporka@gmail.com>
2020-11-13 14:57:23 +0200

    test

--- src/main/java/com/example/sweater/domain/Role.java
+++ src/main/java/com/example/sweater/domain/Role.java
@@ -1,9 +1,10 @@
 package com.example.sweater.domain;
 
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER,ADMIN;
 
     @Override
     public String getAuthority() {
