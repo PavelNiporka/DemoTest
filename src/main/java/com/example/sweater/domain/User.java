5a163d4e0dfce3e8c50f38256fd09db9afbad137
Niporka <pavelniporka@gmail.com>
2020-11-17 17:00:58 +0200

    test

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -10,18 +10,22 @@ import java.util.Set;
 
 @Entity
 @Table(name = "usr")
-public class User  implements UserDetails {
-     @Id
-     @GeneratedValue(strategy = GenerationType.AUTO)
-     private Long id;
-     private String username;
-     private String password;
-     private boolean active;
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private boolean active;
 
-     @ElementCollection(targetClass = Role.class,fetch = FetchType.EAGER)
-     @CollectionTable(name = "user_role",joinColumns = @JoinColumn(name = "user_id"))
-     @Enumerated(EnumType.STRING)
-     private Set<Role> roles;
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
+    }
 
     public Long getId() {
         return id;
